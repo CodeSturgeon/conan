642b58e0ace43062e39ed14df5a10751f87d077f
Nick Fisher <github@nickdafish.com>
2011-06-21 12:22:51 -0400

    now working in a basic way

--- builder.py
+++ builder.py
@@ -6,20 +6,39 @@ import json
 import random
 import string
 import urllib2
+import mimetypes
 import base64
 
 file_name = sys.argv[1]
-# FIXME
-content_type = '!!FIXME!!'
+
+if not os.path.exists(file_name):
+    print 'bad filename'
+    sys.exit(1)
 
 doc = {}
-doc['_attachments'] = {'file_name':{'content_type':content_type}}
+
+with open(file_name) as f:
+    content_type = mimetypes.guess_type(file_name)[0]
+    if content_type == None: content_type = 'application/octet-stream'
+    doc['_attachments'] = {
+            file_name:{
+                'content_type': content_type,
+                'data': base64.b64encode(f.read())
+            }
+    }
 
 token = ''.join(random.choice(string.ascii_letters+string.digits) for x in range(15))
 
 doc['tokens'] = {token: {'generated':'now'} }
 
-doc_id = '????'
+req = urllib2.Request('http://localhost:5984/conan', json.dumps(doc))
+req.add_header('Content-Type', 'application/json')
+resp = urllib2.urlopen(req)
+ret = json.loads(resp.read())
+
+doc_id = ret['id']
+
+print ret
 
 print "http://localhost:5984/conan/_tokened_download/%s?token=%s" % (doc_id, token)
 
