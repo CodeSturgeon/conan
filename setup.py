10fd3841f6dabb0d3d9a9fb9eef600324accf738
Nick Fisher <github@nickdafish.com>
2011-06-22 12:24:59 -0400

    repo cleanup and setuptools init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'conan',
+    version = '0.1dev',
+    description = 'file distribution with couch',
+    author = 'Nick Fisher',
+    packages = find_packages(),
+    zip_safe = False
+)
