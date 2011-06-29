cca3388d66a1eaa353e7b2b4ba5f042071ba8252
Nick Fisher <github@nickdafish.com>
2011-06-29 12:37:17 -0400

    tweaked config doc and implmented it in the dispatcher

--- conan/dispatcher.py
+++ conan/dispatcher.py
@@ -5,34 +5,44 @@ import base64
 import traceback
 import zipfile
 import cStringIO
-
-try:
-    # Python 2.6
-    import json
-except:
-    # Prior to 2.6 requires simplejson
-    import simplejson as json
+import json
 
 # FIXME this should be arg 1, 2 and 3 (host port db_name) and validated
+# FIXME the db name should be taken from the _request_
 db_url = 'http://localhost:5984/conan'
-statics_id = 'static_files'
+statics_id = 'conan_config'
+cfg = {}
 
 class BadToken(Exception):
+    # Signal there is a serious problem with a token
     pass
 
 class UsedToken(Exception):
+    # Signal the token has already beed used
     pass
 
 class NotFound(Exception):
+    # Signal a 404
     pass
 
 def requests():
+    # Iterator for reading couch requests
     line = sys.stdin.readline()
     while line:
         yield json.loads(line)
         line = sys.stdin.readline()
 
+def load_config():
+    # Get config doc from CouchDB
+    try:
+        cfg_ret = urllib2.urlopen("%s/%s"%(db_url,statics_id))
+    except urllib2.HTTPError, e:
+        if e.code == 404: sys.exit('cannot find config doc')
+        raise e
+    return json.loads(cfg_ret.read())
+
 def get_token(token):
+    # Get token data, raising errors if there is a problem with the token
     tok_ret = urllib2.urlopen("%s/_design/conan/_view/tokens?key=\"%s\"&include_docs=true"%(db_url,token))
     tok_ret_json = json.loads(tok_ret.read())
     if len(tok_ret_json['rows']) != 1:
@@ -47,6 +57,7 @@ def get_token(token):
     )
 
 def use_token(tok_doc, addr, token):
+    # Update token in distribution doc
     tok_doc['addresses'][addr]['tokens'][token]['use_time'] = 'now'
     tok_upd = urllib2.Request("%s/%s"%(db_url,tok_doc['_id']))
     tok_upd.add_data(json.dumps(tok_doc))
@@ -62,7 +73,11 @@ def compile_zip(doc_id, files):
     # Compile zip
     for file_name in files:
         # Get file from Couch
-        fdata = urllib2.urlopen('%s/%s/%s'%(db_url,doc_id,file_name)).read()
+        try:
+            fdata = urllib2.urlopen('%s/%s/%s'%(db_url,doc_id,file_name)).read()
+        except urllib2.HTTPError, e:
+            if e.code == 404: raise BadToken
+            raise e
         # Add file to zip
         z.writestr(file_name, fdata)
     # Finish zipping
@@ -80,7 +95,9 @@ def get_file(name):
     return ctype, base64.b64encode(file_ret.read())
 
 def main():
+    cfg = load_config()
     for req in requests():
+        if cfg['dispatcher']['reload_config']: cfg = load_config()
         ret = {'headers': {}}
         try:
             if len(req['path']) < 3: raise BadToken
@@ -95,23 +112,38 @@ def main():
                 b64_data = compile_zip(pub_id, file_names)
                 use_token(tok_doc, addr, token)
                 ret['headers']['Content-Type'] = 'application/zip'
-                ret['headers']['Content-Disposition'] = "attachment; filename=%s.zip"%token
-            ret['base64'] = b64_data
+                c_disp = "attachment; filename=%s.zip" % token
+                ret['headers']['Content-Disposition'] = c_disp
             ret['code'] = 200
+            ret['base64'] = b64_data
         except BadToken:
             ret["code"] = 401
-            ret["body"] = 'bad'
+            ctype, b64 = get_file('bad_token.html')
+            ret['headers']['Content-Type'] = ctype
+            ret['base64'] = b64
         except UsedToken:
             ret["code"] = 403
-            ret["body"] = 'used'
+            ctype, b64 = get_file('used_token.html')
+            ret['headers']['Content-Type'] = ctype
+            ret['base64'] = b64
         except NotFound:
             ret['code'] = 404
-            ret['body'] = 'not found'
+            ctype, b64 = get_file('404.html')
+            ret['headers']['Content-Type'] = ctype
+            ret['base64'] = b64
         except Exception as e:
-            trace = "\n".join(traceback.format_tb(sys.exc_info()[2]))
             ret["code"] = 500
-            ret["body"] = "%s\n\n%s\n"%(e,trace)
-            ret['headers']['Content-Type'] = 'text/plain'
+            if cfg['dispatcher']['allow_debug'] and req['query'].has_key('debug'): 
+                body = "\n".join(traceback.format_tb(sys.exc_info()[2]))
+                body += "\nCFG:\n%s\n" % json.dumps(cfg,indent=2)
+                body += "\nREQ:\n%s\n" % json.dumps(req,indent=2)
+                ret["body"] = "%s\n\n%s\n"%(e,body)
+                ret['headers']['Content-Type'] = 'text/plain'
+            else:
+                ctype, b64 = get_file('500.html')
+                ret['headers']['Content-Type'] = ctype
+                ret['base64'] = b64
+
         sys.stdout.write("%s\n" % json.dumps(ret))
         sys.stdout.flush()
 
