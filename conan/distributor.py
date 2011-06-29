326aef0fd35494d6f9e14d121e4e12725d7b2712
Nick Fisher <github@nickdafish.com>
2011-06-29 15:19:51 -0400

    adding email support to distributor

--- conan/distributor.py
+++ conan/distributor.py
@@ -5,6 +5,7 @@ import re
 import json
 import datetime
 from conan import gen_token
+import conan.email
 import sys
 from optparse import OptionParser
 
@@ -34,7 +35,10 @@ def main():
     db_url = "http://%s:%s/%s"%(options.db_host, options.db_port,
                                 options.db_name)
 
-    name = 'Feature Pending'
+    name = '_name feature pending_'
+
+    # Load config... currently only used for email
+    cfg = json.loads(urllib2.urlopen("%s/conan_config"%db_url).read())
 
     # get list of files in that publication
     pub_files = {}
@@ -42,8 +46,6 @@ def main():
     files_view_url  = "%s/_design/conan/_view/publication_files"%db_url
     files_view_url += "?startkey=[\"%s\",]&endkey=[\"%s\",{}]"%(pub_id,pub_id)
     files_view_ret = json.loads(urllib2.urlopen(files_view_url).read())
-    print files_view_url
-    print files_view_ret
 
     if len(files_view_ret['rows']) == 0:
         print 'publication not found'
@@ -91,6 +93,9 @@ def main():
         print 'Something went wrong with the creation of the dist doc...'
         print ret
         sys.exit(2)
+
+    conan.email.configure(cfg)
+    conan.email.send_token(email ,name, token)
 
 if __name__ == "__main__":
     main()
