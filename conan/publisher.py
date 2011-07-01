2b0f6cb580522d86f7f517e7816693a5b30c8469
Nick Fisher <github@nickdafish.com>
2011-07-01 18:49:20 -0400

    first pass at restating... needs better testing

--- conan/publisher.py
+++ conan/publisher.py
@@ -9,6 +9,8 @@ import hashlib
 import urllib2
 import json
 from optparse import OptionParser
+from conan import gen_token
+import conan.email
 
 # list files and md5
 def load_file(path):
@@ -30,7 +32,12 @@ def main():
                       help='Port of the CouchDB server')
     parser.add_option('--db', dest='db_name', default='conan',
                       help='Database to use in CouchDB')
+    parser.add_option('-r', '--restate', dest='restate', default=False,
+                      action='store_true',
+                      help='Restate files rather than publish as new')
     (options, args) = parser.parse_args()
+
+    iso_now = datetime.datetime.utcnow().isoformat()+'Z'
 
     # validate args
     if len(args) != 1:
@@ -60,32 +67,137 @@ def main():
                 f_vpath = f_path[len(source_path):]
                 pub_files[f_vpath] = load_file(f_path)
 
-    # build metadata
-    doc = {
-        'doc_type': 'publication',
-        'versions': [
-            {
-                'files': dict([(f,pub_files[f]['digest']) for f in pub_files]),
-                'source_path': source_path,
-                'create_time': datetime.datetime.utcnow().isoformat()+'Z',
-                'message': 'initial creation',
-                'stub': 'test_pub'
-            }
-        ],
-        '_attachments': pub_files
-    }
+    # search for and warn about files that start with a _
+    # CouchDB has inconsistant handling of these files
+    # https://issues.apache.org/jira/browse/COUCHDB-1210
+    for filename in pub_files.keys():
+        if filename.startswith('_'):
+            print 'leading underscores are not allowed:', filename
+            sys.exit(2)
 
-    req = urllib2.Request(db_url, json.dumps(doc))
-    req.add_header('Content-Type', 'application/json')
-    resp = urllib2.urlopen(req)
-    ret = json.loads(resp.read())
-    if resp.code == 201:
-        print 'Files published'
-        print "Publication ID:", ret['id']
-    else:
-        print 'Something went wrong with the publish...'
-        print ret
+    # look for the files in previous publications
+    files_req = urllib2.Request("%s/_design/conan/_view/published_files"%db_url)
+    files_req.get_method = lambda: 'POST'
+    files_req.add_data(json.dumps({"keys":pub_files.keys()}))
+    files_req.add_header('Content-Type', 'application/json')
+    files_ret = json.loads(urllib2.urlopen(files_req).read())
+    files_previous = files_ret['rows']
+
+    if not options.restate:
+        # Take care of easyiest case first
+        if len(files_previous) != 0:
+            print 'some files have been previously published - aborting'
+            sys.exit(2)
+        # build metadata
+        doc = {
+            'doc_type': 'publication',
+            'versions': [
+                {
+                    'files': dict([(f,pub_files[f]['digest']) for f in pub_files]),
+                    'source_path': source_path,
+                    'create_time': iso_now,
+                    'message': 'initial creation',
+                    'stub': 'test_pub'
+                }
+            ],
+            '_attachments': pub_files
+        }
+        print json.dumps(doc, indent=2); sys.exit()
+        req = urllib2.Request(db_url, json.dumps(doc))
+        req.add_header('Content-Type', 'application/json')
+        resp = urllib2.urlopen(req)
+        ret = json.loads(resp.read())
+        if resp.code == 201:
+            print 'Files published'
+            print "Publication ID:", ret['id']
+            sys.exit()
+        else:
+            print 'Something went wrong with the publish...'
+            print ret
+            sys.exit(2)
+
+    # If we got here, we are restating files
+
+    # if pub_files not in files_previous raise error
+    # if pub_files spans publicatiuons raise error
+    pub_keys = pub_files.keys()
+    upd_pub = ''
+
+    # Publication checking
+    for prev_data in files_previous:
+        if upd_pub == '':
+            upd_pub = prev_data['id']
+        if upd_pub != prev_data['id']:
+            print 'files span publications'
+            sys.exit(2)
+
+        pub_keys.remove(prev_data['key'])
+
+    if len(pub_keys) != 0:
+        print 'some files not found in previous publication!'
+        print pub_keys
         sys.exit(2)
+
+    # consult md5s to see what changed
+
+    # ammend publication
+    pub_req = urllib2.Request("%s/%s"%(db_url,upd_pub))
+    pub = json.loads(urllib2.urlopen(pub_req).read())
+    pub['_attachments'].update(pub_files)
+
+    pub_req.get_method = lambda: 'PUT'
+    pub_req.add_data(json.dumps(pub))
+    pub_req.add_header('Content-Type', 'application/json')
+    pub_ret = urllib2.urlopen(pub_req)
+
+    # find who has downloaded these files
+    dls_req = urllib2.Request("%s/_design/conan/_view/downloaded_files?group=true"%db_url)
+    dls_req.get_method = lambda: 'POST'
+    dls_req.add_data(json.dumps({"keys":pub_files.keys()}))
+    dls_req.add_header('Content-Type', 'application/json')
+    dls_ret = json.loads(urllib2.urlopen(dls_req).read())
+    dls_infos = dls_ret['rows']
+
+    # get tokens to anyone who downloaded the restated files
+    if len(dls_infos) != 0:
+        addrs = {}
+
+        # generate file lists and tokens
+        for dl_info in dls_infos:
+            filename = dl_info['key']
+            for addr_data in dl_info['value']:
+                addr = addr_data[0]
+                if not addrs.has_key(addr):
+                    addrs[addr] = {
+                        'name': addr_data[1],
+                        'tokens': {
+                            gen_token(): {
+                                'gen_time': iso_now
+                            }
+                        },
+                        'files': []
+                    }
+                addrs[addr]['files'].append(filename)
+
+
+        # make new distribution doc
+        doc = {
+            'doc_type': 'distribution',
+            'publication_id': upd_pub,
+            'addresses': addrs
+        }
+        req = urllib2.Request(db_url, json.dumps(doc))
+        req.add_header('Content-Type', 'application/json')
+        resp = urllib2.urlopen(req)
+        ret = json.loads(resp.read())
+        # FIXME check return
+
+        # send token notifications
+        cfg = json.loads(urllib2.urlopen("%s/conan_config"%db_url).read())
+        conan.email.configure(cfg)
+        for addr in addrs:
+            token = addrs[addr]['tokens'].itervalues().next()
+            conan.email.send_token(addr, addrs[addr]['name'], token)
 
 if __name__ == "__main__":
     main()
